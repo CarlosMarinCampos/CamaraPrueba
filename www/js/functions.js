c60ebf1551d606789ec1febc3b729e944c39eb51
CarlosMarinCampos <car.marin07@gmail.com>
2017-02-03 19:36:53 +0100

    1.1
    Poner alerts para comprobar funcionamiento.

--- www/js/functions.js
+++ www/js/functions.js
@@ -6,16 +6,21 @@ document.addEventListener("deviceready",onDeviceReady,false);
 
 //Cuando las API están disponibles, coger los valores
 function onDeviceReady() {
+	alert('Entro en el onDeviceReady');
     destinoImagen=navigator.camera.PictureSourceType;
     formatoValor=navigator.camera.DestinationType;
+    alert('Igual he hecho lo del onDeviceReady');
 }
 
 /** CAPTURAR FOTO **/
 function capturarFoto(){
+	alert('Estoy en el capturarFoto');
 	navigator.camera.getPicture(onPhotoDataSuccess, mostrarError, { quality: 50, destinationType: formatoValor.DATA_URL });
+	alert('Igual he hecho lo de capturarFoto');
 }
 
 function onPhotoDataSuccess(datosImagen) {
+	alert('Estoy en el onPhotoDataSuccess');
 	//console.log(datosImagen); //Ver los datos de la imagen codificados en base64
 
     //Coger el mango de la foto
@@ -27,6 +32,7 @@ function onPhotoDataSuccess(datosImagen) {
     //Mostrar la foto capturada
     //Las reglas CSS de la linea se usan para redimensionar la imagen
     fotoPequena.src="data:image/jpeg;base64,"+datosImagen;
+    alert('Igual he hecho lo de onPhotoDataSuccess');
 }
 
 /** Mostrar Error **/
